fa5ddf09036e61b60c491df1b1ea399572dd6eb3
Brandon Rodriguez <chilebr99@gmail.com>
2023-04-16 23:47:55 -0400

    added primary balance css

--- scripts/crypto-detail.js
+++ scripts/crypto-detail.js
@@ -1,15 +1,17 @@
 // Toggles hamburger overlay
 document.querySelector(".hamburger").addEventListener("click", function () {
-    document.querySelector("nav").classList.toggle("show");
-  });
+  document.querySelector("nav").classList.toggle("show");
+});
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const coinName = urlParams.get("coin")
+const urlParams = new URLSearchParams(window.location.search);
+const coinName = urlParams.get("coin");
 
-  fetch(`https://api.coingecko.com/api/v3/coins/${coinName}?localization=false&tickers=false&developer_data=false&sparkline=false`)
+fetch(
+  `https://api.coingecko.com/api/v3/coins/${coinName}?localization=false&tickers=false&developer_data=false&sparkline=false`
+)
   .then((res) => res.json())
   .then((data) => {
-    const coinDetailsElement = document.getElementById("coin-details")
+    const coinDetailsElement = document.getElementById("coin-details");
     coinDetailsElement.innerHTML = `
    <h1>${data.name} Price</h1>
    <section class="coin-header">
@@ -17,22 +19,35 @@ document.querySelector(".hamburger").addEventListener("click", function () {
    <p>$${data.market_data.current_price.usd.toLocaleString()}</p>
    <button><i class="fa-solid fa-star"></i></button>
    </div>
-   <p class="${data.market_data.price_change_24h >= 0 ? "positive" : "negative"}">$${data.market_data.price_change_24h.toFixed(2)} (${data.market_data.price_change_percentage_24h.toFixed(2)}%)</p>
+   <p class="${
+     data.market_data.price_change_24h >= 0 ? "positive" : "negative"
+   }">$${data.market_data.price_change_24h.toFixed(
+      2
+    )} (${data.market_data.price_change_percentage_24h.toFixed(2)}%)</p>
    </section>
 <section class="coin-graph"></section>
 <section class="coin-info">
-<p>Your balance</p>
-<p>$100,000<i class="fa-solid fa-greater-than"></i><p>
+<div class="your-balance">
+<p class="balance-title">Your balance</p>
+<p class="balance">$100,000</p>
 <div class="coin-balance">
 <div class="coin-primary-balance">
+<img src="${data.image.small}" alt="${data.name}">
+<div class="coin-text">
 <h2>${data.name}</h2>
 <p>Primary balance</p>
+</div>
+
 </div>
 <div class="coin-shares">
 <p>$10,000</p>
 <p>$-1,304</p>
 </div>
 </div>
+</div>
+
+
+
 <section class="about-coin">
 <p>About ${data.name}</p>
 <p>${data.description.en}</p>
