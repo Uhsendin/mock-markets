691a18f3e28f26dd91daf8a47ece3f5d1605268b
Uhsendin <chilebr99@gmail.com>
2023-05-16 13:28:33 -0400

    Created function to calculate current time and past 23 hours

--- scripts/stock-chart.js
+++ scripts/stock-chart.js
@@ -1,32 +1,19 @@
-const selectedCryptos = localStorage.getItem("selectedCryptos");
+const generateDayArray = () => {
+  const currentTime = new Date()
+  const dayArray = []
+  
+  for (let i = 0; i < 24; i++) {
+    const hour = currentTime.getHours()
+    const minute = currentTime.getMinutes()
+    const timeStr = hour + ":" + (minute < 10 ? "0" : "") + minute
+    dayArray.unshift(timeStr)
+    // currentTime.setHours(hour -1)
+  }
+  return dayArray
+}
+console.log(generateDayArray())
 
-const labels = [
-  "00:00",
-  "01:00",
-  "02:00",
-  "03:00",
-  "04:00",
-  "05:00",
-  "06:00",
-  "07:00",
-  "08:00",
-  "09:00",
-  "10:00",
-  "11:00",
-  "12:00",
-  "13:00",
-  "14:00",
-  "15:00",
-  "16:00",
-  "17:00",
-  "18:00",
-  "19:00",
-  "20:00",
-  "21:00",
-  "22:00",
-  "23:00",
-  "24:00"
-];
+const labels = generateDayArray();
 
 const data = {
   labels: labels,
