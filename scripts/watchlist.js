f6c59aa79e02385ac724caf76da63f333485cb5c
Uhsendin <chilebr99@gmail.com>
2023-05-25 10:56:41 -0400

    fetch call wont run if there is no coins saved in watchlist

--- scripts/watchlist.js
+++ scripts/watchlist.js
@@ -1,95 +1,95 @@
 import { data, config } from "./stock-chart.js";
 
-// Retrieve the value of "selectedCryptos" from localStorage
-const selectedCryptos = localStorage.getItem("selectedCryptos");
-const watchListContainer = document.querySelector(".watchlist-container");
-const watchList = [];
-// Check if the value is null or not a valid JSON string
-if (selectedCryptos === null) {
-  console.log("selectedCryptos does not exist in localStorage");
-} else {
-  try {
-    //try to parse the value as JSON
-    const parsedCryptos = JSON.parse(selectedCryptos);
-    // Push Crypto id to array
-    parsedCryptos.forEach((crypto) => {
-      watchList.push(crypto);
-    });
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
+const fetchData = async () => {
+  // Retrieve the value of "selectedCryptos" from localStorage
+  const selectedCryptos = localStorage.getItem("selectedCryptos");
+  const watchListContainer = document.querySelector(".watchlist-container");
+  const watchList = [];
+  // Check if the value is null or not a valid JSON string
+  if (selectedCryptos === null) {
+    console.log("selectedCryptos does not exist in localStorage")
+
+  } else {
+    try {
+      // Try to parse the value as JSON
+      const parsedCryptos = JSON.parse(selectedCryptos);
+      // Push Crypto id to array
+      parsedCryptos.forEach((crypto) => {
+        watchList.push(crypto);
+      });
+
+      const coinIds = watchList.map((coin) => coin.id);
+
+      // Construct the URL for the batch request
+      const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds.join(
+        ","
+      )}&localization=false&tickers=false&developer_data=false&sparkline=true`;
+
+      const response = await fetch(url);
+      const coinData = await response.json();
+
+      coinData.forEach((info) => {
+        const coinPriceString = info.current_price.toLocaleString(undefined, {
+          maximumFractionDigits: 3,
+          minimumFractionDigits: 3,
+        });
+        const coin24High = info.price_change_percentage_24h;
+        const sparklineList = info.sparkline_in_7d.price;
+        const sparkLineCurrentDay = sparklineList.slice(-24);
+
+        const coinContainer = document.createElement("section");
+        coinContainer.classList.add("coin");
+        coinContainer.innerHTML = `
+          <div class="coin-info">
+            <div class="coin-img">
+              <img src="${info.image}">
+            </div>
+            <div class="coin-name">
+              <h2>${info.name}</h2>
+              <span>${info.symbol}</span>
+            </div>
+          </div>
+          <div class="canvas-container">
+            <canvas class="stock-chart"></canvas>
+          </div>
+          <div class="coin-prices">
+            <span class="current-price">$${coinPriceString}</span>
+            <span class="price-24-high ${
+              coin24High >= 0 ? "positive" : "negative"
+            }">
+              <i class="fas ${
+                coin24High >= 0
+                  ? "fa-solid fa-arrow-trend-up positive"
+                  : "fa-solid fa-arrow-trend-down negative"
+              }"></i> ${coin24High.toFixed(3)}%
+            </span>
+          </div>
+        `;
+
+        coinContainer.addEventListener("click", () => {
+          window.location.href = `/pages/crypto-details.html?coin=${info.id}`;
+        });
+
+        watchListContainer.appendChild(coinContainer);
+
+        coinContainer.querySelectorAll(".stock-chart").forEach((canvas) => {
+          if (canvas) {
+            data.datasets[0].data = sparkLineCurrentDay;
+            data.datasets[0].borderColor = coin24High < 0 ? "red" : "green";
+
+            const ctx = canvas.getContext("2d");
+            const existingChart = Chart.getChart(ctx);
+            if (existingChart) {
+              existingChart.destroy();
+            }
+            new Chart(ctx, config);
+          }
+        });
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
-}
+};
 
-const coinIds = watchList.map((coin) => coin.id);
-
-//Render coin details from api
-try {
-  // Construct the URL for the batch request
-  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds.join(
-    ","
-  )}&localization=false&tickers=false&developer_data=false&sparkline=true`;
-
-  const response = await fetch(url);
-  const coinData = await response.json();
-
-  coinData.forEach((info) => {
-    const coinPriceString = info.current_price.toLocaleString(undefined, {
-      maximumFractionDigits: 3,
-      minimumFractionDigits: 3,
-    });
-    const coin24High = info.price_change_percentage_24h;
-    const sparklineList = info.sparkline_in_7d.price;
-    const sparkLineCurrentDay = sparklineList.slice(-24);
-
-    const coinContainer = document.createElement("section");
-    coinContainer.classList.add("coin");
-    coinContainer.innerHTML = `
-        <div class="coin-info">
-          <div class="coin-img">
-            <img src="${info.image}">
-          </div>
-          <div class="coin-name">
-            <h2>${info.name}</h2>
-            <span>${info.symbol}</span>
-          </div>
-        </div>
-        <div class="canvas-container">
-          <canvas class="stock-chart"></canvas>
-        </div>
-        <div class="coin-prices">
-          <span class="current-price">$${coinPriceString}</span>
-          <span class="price-24-high ${
-            coin24High >= 0 ? "positive" : "negative"
-          }">
-            <i class="fas ${
-              coin24High >= 0
-                ? "fa-solid fa-arrow-trend-up positive"
-                : "fa-solid fa-arrow-trend-down negative"
-            }"></i> ${coin24High.toFixed(3)}%
-          </span>
-        </div>
-      `;
-
-    coinContainer.addEventListener("click", () => {
-      window.location.href = `/pages/crypto-details.html?coin=${info.id}`;
-    });
-
-    watchListContainer.appendChild(coinContainer);
-
-    coinContainer.querySelectorAll(".stock-chart").forEach((canvas) => {
-      if (canvas) {
-        data.datasets[0].data = sparkLineCurrentDay;
-        data.datasets[0].borderColor = coin24High < 0 ? "red" : "green";
-
-        const ctx = canvas.getContext("2d");
-        const existingChart = Chart.getChart(ctx);
-        if (existingChart) {
-          existingChart.destroy();
-        }
-        new Chart(ctx, config);
-      }
-    });
-  });
-} catch (error) {
-  console.error("Error fetching coin info:", error);
-}
+fetchData();
