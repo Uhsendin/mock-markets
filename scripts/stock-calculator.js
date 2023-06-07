d7cb9f06d737704d10a11d87059dea09ce0e47f7
Brandon Rodriguez <chilebr99@gmail.com>
2023-06-07 14:01:00 -0400

    input width is dynamic

--- scripts/stock-calculator.js
+++ scripts/stock-calculator.js
@@ -1,19 +1,33 @@
-const coinDetails = localStorage.getItem("tempCoinInfo")
-const coinDetailsParsed = JSON.parse(coinDetails)
-const coinShareSymbol = document.querySelector(".coin-share span")
-const image = document.querySelector(".upper img")
-const coinName = document.querySelector(".coin-name")
-let shareNumAmount = 0
+const coinDetails = localStorage.getItem("tempCoinInfo");
+const coinDetailsParsed = JSON.parse(coinDetails);
+const coinShareSymbol = document.querySelector(".coin-share span");
+const image = document.querySelector(".upper img");
+const coinName = document.querySelector(".coin-name");
+let shareNumAmount = 0;
+const numValue = document.querySelector(".amount input");
+numValue.style.width = "33px"
 
-coinShareSymbol.textContent = `${shareNumAmount} ${coinDetailsParsed.symbol.toUpperCase()}`
-image.src = coinDetailsParsed.image
-coinName.textContent = coinDetailsParsed.name
+coinShareSymbol.textContent = `${shareNumAmount} ${coinDetailsParsed.symbol.toUpperCase()}`;
+image.src = coinDetailsParsed.image;
+coinName.textContent = coinDetailsParsed.name;
 if (coinDetailsParsed.buy) {
-    document.getElementById("buy").classList.add("active-pill")
+  document.getElementById("buy").classList.add("active-pill");
 } else {
-    document.getElementById("sell").classList.add("active-pill")
+  document.getElementById("sell").classList.add("active-pill");
 }
 
+numValue.addEventListener("input", () => {
+    const valueLength = numValue.value.length;
+    const minWidth = 33; // Minimum width of the input
+    
+    if (valueLength === 1) {
+      numValue.style.width = "33px";
+    } else {
+      numValue.style.width = `${minWidth * valueLength}px`;
+    }
+  });
+  
+
 document.querySelector(".order-header a").addEventListener("click", () => {
-    window.location.href = `/pages/crypto-details.html?coin=${coinDetailsParsed.id}`;
-  })
+  window.location.href = `/pages/crypto-details.html?coin=${coinDetailsParsed.id}`;
+});
